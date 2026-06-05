cc9e869d17e637191e0ff3bee96aa68875c18b09
Joao Gabriel <joaogabrielsh@gmail.com>
2026-06-05 11:56:25 -0300

    first commit

---
+++ src/main/java/com/joaog4briel/ninjas/NinjasApplication.java
@@ -0,0 +1,13 @@
+package com.joaog4briel.ninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaog4briel/ninjas/NinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaog4briel.ninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
