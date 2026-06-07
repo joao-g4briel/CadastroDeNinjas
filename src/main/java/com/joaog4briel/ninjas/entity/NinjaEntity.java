c5dd945aa4bc842de420e1f58a9226023388beff
Joao Gabriel <joaogabrielsh@gmail.com>
2026-06-07 16:56:25 -0300

    Cria uma entidade no banco de dados a partir do NinjaEntity

---
+++ src/main/java/com/joaog4briel/ninjas/entity/NinjaEntity.java
@@ -0,0 +1,46 @@
+package com.joaog4briel.ninjas.entity;
+
+import jakarta.persistence.*;
+
+@Entity //Cria a entidade no banco de dados
+@Table (name = "tb_cadastro") //Da o nome da tabela no banco de dados
+public class NinjaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaEntity() {}
+
+    public NinjaEntity(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
